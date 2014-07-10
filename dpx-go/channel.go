e1508862e1c7b3d11b380c7ac24ad05515cdb0fd
Robert Xu <robxu9@gmail.com>
2014-07-10 18:10:58 -0400

    dpx-go: convert channels & peer to cgo, remove conn
    dpx_duplex_conn isn't exposed and is purely internal

--- dpx-go/channel.go
+++ dpx-go/channel.go
@@ -4,10 +4,7 @@ package dpx
 // #include <dpx.h>
 import "C"
 
-import (
-	"errors"
-	"runtime"
-)
+import "runtime"
 
 var ChannelQueueHWM = 1024
 
@@ -20,6 +17,11 @@ func fromCChannel(ch *C.dpx_channel) *Channel {
 	runtime.SetFinalizer(channel, func(x *Channel) {
 		C.dpx_channel_free(x.ch)
 	})
+	return channel
+}
+
+func (c *Channel) Method() string {
+	return C.GoString(C.dpx_channel_method_get(c.ch))
 }
 
 func (c *Channel) Error() error {
@@ -35,71 +37,5 @@ func (c *Channel) ReceiveFrame() *Frame {
 }
 
 func (c *Channel) SendFrame(frame *Frame) error {
-	c.Lock()
-	defer c.Unlock()
-	if c.err != nil {
-		return c.err
-	}
-	if c.closed {
-		return errors.New("duplex: channel is closed")
-	}
-	frame.chanRef = c
-	frame.Channel = c.id
-	frame.Type = DataFrame
-	c.outgoing <- frame
-	return nil
-}
-
-func (c *Channel) HandleIncoming(frame *Frame) bool {
-	c.Lock()
-	defer c.Unlock()
-	if c.closed {
-		return false
-	}
-	if !frame.Last && frame.Error != "" {
-		go c.close(errors.New(frame.Error))
-		return true
-	}
-	c.incoming <- frame
-	return true
-}
-
-func (c *Channel) pumpOutgoing() {
-	var stillopen bool
-	c.conn = <-c.connCh
-	debug(c.peer.index, "Pumping started for channel", c.id)
-	defer debug(c.peer.index, "Pumping finished for channel", c.id)
-	for {
-		select {
-		case c.conn, stillopen = <-c.connCh:
-			if !stillopen {
-				c.conn = nil
-				return
-			}
-		case frame, ok := <-c.outgoing:
-			if !ok {
-				return
-			}
-			for {
-				debug(c.peer.index, "Sending frame:", frame)
-				err := c.conn.writeFrame(frame)
-				if err != nil {
-					debug(c.peer.index, "Error sending frame:", err)
-					c.conn, stillopen = <-c.connCh
-					debug(c.peer.index, "New connection for channel", c.id)
-					if !stillopen {
-						c.conn = nil
-						return
-					}
-					continue
-				}
-				if frame.Error != "" {
-					c.close(errors.New(frame.Error))
-				} else if frame.Last && c.server {
-					c.close(nil)
-				}
-				break
-			}
-		}
-	}
+	return ParseError(uint64(C.dpx_channel_send_frame(c.ch, toCFrame(frame))))
 }
--- dpx-go/peer.go
+++ dpx-go/peer.go
@@ -1,196 +1,83 @@
 package dpx
 
+// #cgo LDFLAGS: -ldpx
+// #include <dpx.h>
+// #include <stdlib.h>
+import "C"
+
 import (
-	"errors"
 	"net"
-	"sync"
-	"time"
+	"runtime"
+	"strconv"
 )
 
-var peerIndex = 0
-
 type Peer struct {
-	sync.Mutex
-	listeners        []net.Listener
-	conns            []*duplexconn
-	openFrames       chan *Frame
-	incomingChannels chan *Channel
-	closed           bool
-	rrIndex          int
-	chanIndex        int
-	index            int
-	firstConn        chan struct{}
+	peer *C.dpx_peer
 }
 
 func newPeer() *Peer {
-	s := &Peer{
-		index:            peerIndex,
-		conns:            make([]*duplexconn, 0),
-		openFrames:       make(chan *Frame, ChannelQueueHWM),
-		incomingChannels: make(chan *Channel, 1024),
-		firstConn:        make(chan struct{}),
-	}
-	peerIndex += 1
-	go s.routeOpenFrames()
-	return s
-}
+	context := C.dpx_init()
+	peer := C.dpx_peer_new(context)
 
-func (p *Peer) acceptConnection(conn net.Conn) {
-	dc := &duplexconn{
-		peer:     p,
-		conn:     conn,
-		writeCh:  make(chan *Frame),
-		channels: make(map[int]*Channel),
+	p := &Peer{
+		peer: peer,
 	}
-	p.Lock()
-	p.conns = append(p.conns, dc)
-	if len(p.conns) == 1 {
-		p.firstConn <- struct{}{}
-	}
-	p.Unlock()
-	go dc.readFrames()
-	go dc.writeFrames()
-}
 
-func (s *Peer) nextConn() (*duplexconn, int) {
-	index := s.rrIndex % len(s.conns)
-	s.Lock()
-	conn := s.conns[index]
-	s.rrIndex += 1
-	s.Unlock()
-	return conn, index
-}
+	runtime.SetFinalizer(p, func(p *Peer) {
+		C.dpx_peer_close(p)
+		C.dpx_peer_free(p)
+		C.dpx_cleanup(context)
+	})
 
-func (s *Peer) routeOpenFrames() {
-	var err error
-	var frame *Frame
-	var ok bool
-	for {
-		<-s.firstConn
-		debug(s.index, "First connection, routing...")
-		for len(s.conns) > 0 {
-			if err == nil {
-				frame, ok = <-s.openFrames
-				if !ok {
-					return
-				}
-			}
-			conn, index := s.nextConn()
-			debug(s.index, "Sending frame [", index, "]:", frame)
-			err = conn.writeFrame(frame)
-			if err == nil {
-				conn.LinkChannel(frame.chanRef)
-			}
-		}
-	}
+	return p
 }
 
 func (p *Peer) Open(method string) *Channel {
-	p.Lock()
-	defer p.Unlock()
-	if p.closed {
-		return nil
-	}
-	channel := newClientChannel(p, method)
-	frame := newFrame(channel)
-	frame.Type = OpenFrame
-	frame.Method = method
-	p.openFrames <- frame
-	return channel
-}
-
-func (p *Peer) HandleOpen(conn *duplexconn, frame *Frame) bool {
-	p.Lock()
-	defer p.Unlock()
-	if p.closed {
-		return false
-	}
-	p.incomingChannels <- newServerChannel(conn, frame)
-	return true
+	cMethod := C.CString(method)
+	defer C.free(cMethod)
+	cChan := C.dpx_peer_open(p.peer, cMethod)
+	return fromCChannel(cChan)
 }
 
 func (p *Peer) Accept() *Channel {
-	channel, ok := <-p.incomingChannels
-	if !ok {
-		return nil
-	}
-	return channel
+	cChan := C.dpx_peer_accept(p.peer)
+	return fromCChannel(cChan)
 }
 
 func (p *Peer) Close() error {
-	p.Lock()
-	defer p.Unlock()
-	if p.closed {
-		return errors.New("duplex: peer already closed")
-	}
-	p.closed = true
-	close(p.openFrames)
-	close(p.incomingChannels)
-	for _, listener := range p.listeners {
-		if err := listener.Close(); err != nil {
-			return err
-		}
-	}
-	for _, conn := range p.conns {
-		if err := conn.conn.Close(); err != nil {
-			return err
-		}
-	}
-	return nil
+	return ParseError(uint64(C.dpx_peer_close(p.peer)))
 }
 
 func (p *Peer) Connect(addr string) error {
-	p.Lock()
-	defer p.Unlock()
-	if p.closed {
-		return errors.New("duplex: peer is closed")
-	}
-	go func() {
-		for i := 0; i < RetryAttempts; i++ {
-			debug(p.index, "Connecting", addr)
-			conn, err := net.Dial("tcp", addr)
-			if err != nil {
-				debug(p.index, "Unable to connect, retrying soon...")
-				time.Sleep(time.Duration(RetryWaitSec) * time.Second)
-				continue
-			}
-			if err := sendGreeting(conn); err != nil {
-				debug("failed to make greeting")
-				return
-			}
-			debug(p.index, "Connected")
-			p.acceptConnection(conn)
-			return
-		}
-	}()
-	return nil
-}
-
-func (p *Peer) Bind(addr string) error {
-	p.Lock()
-	defer p.Unlock()
-	if p.closed {
-		return errors.New("duplex: peer is closed")
-	}
-	listener, err := net.Listen("tcp", addr)
+	host, port, err := net.SplitHostPort(addr)
 	if err != nil {
 		return err
 	}
-	p.listeners = append(p.listeners, listener)
-	debug(p.index, "Now listening", addr)
-	go func() {
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				debug(err)
-				return
-			}
-			go func() {
-				if receiveGreeting(conn) {
-					p.acceptConnection(conn)
-				}
-			}()
-		}
-	}()
-	return nil
+
+	iport, err := strconv.Atoi(port)
+	if err != nil {
+		return err
+	}
+
+	chost := C.CString(host)
+	defer C.free(host)
+
+	return ParseError(C.dpx_peer_connect(p.peer, chost, C.int(iport)))
+}
+
+func (p *Peer) Bind(addr string) error {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return err
+	}
+
+	iport, err := strconv.Atoi(port)
+	if err != nil {
+		return err
+	}
+
+	chost := C.CString(host)
+	defer C.free(host)
+
+	return ParseError(C.dpx_peer_bind(p.peer, chost, C.int(iport)))
 }
