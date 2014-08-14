1e9b5f60496940701aec7d34c0fd3505132b3c43
Robert Xu <robxu9@gmail.com>
2014-08-14 13:26:38 -0400

    Revert "prototype: use UUID v1"
    This reverts commit 725b99652958750ea3dc5df72d4de287a8c9c593.
    
    Conflicts:
    	prototype/dpx/peer.go

--- prototype/dpx/peer.go
+++ prototype/dpx/peer.go
@@ -32,7 +32,7 @@ func newPeer() *Peer {
 		openFrames:       make(chan *Frame, ChannelQueueHWM),
 		incomingChannels: make(chan *Channel, 1024),
 		firstConn:        make(chan struct{}),
-		uuid:             uuid.NewUUID().String(),
+		uuid:             uuid.New(),
 	}
 	peerIndex += 1
 	go s.routeOpenFrames()
